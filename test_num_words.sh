d592ff64344bbe7c5039f2aaf04d76009c28df1f
Zachary Kagan <zakagan@gmail.com>
2016-06-21 02:43:55 -0500

    updated tests

--- test_num_words.sh
+++ test_num_words.sh
@@ -3,35 +3,33 @@ clear
 
 file_name="results_num_words.txt"
 
+base_strings=("ougzvhsrwf" "ruyajlwigx" "wwufpxuwrg" "bznpflexir" "hqayyznmxr")
+
 touch $file_name
 
-echo "Testing N: file length by word count
-ten char strings: 
-hSF9RITZYq
-OHx1ZEHiht
-9cBqvgN0Qv
-c0FnTkWjuL
-Yl69pHrgvy
-"  1> $file_name
+echo "Testing N: file length by word count w/ ten char base strings"  1> $file_name
+
+echo "" 1>> $file_name
+for string in "${base_strings[@]}"; do
+	echo $string 1>> $file_name
+done
+echo "" 1>> $file_name
 
 for executable in ./WordPercentSort ./WordPercentTable ./WordPercentPrecomputeSort ./WordPercentPrecomputeTable; do
 
-	echo "
-	$executable" 1>> $file_name
+	echo "" 1>> $file_name
+	echo "$executable" 1>> $file_name
 
 	for i in "" "2" "4" "8" "16" "32" "64" "128" "256" "512"; do
 
-		echo "
-		file: words$i
-		" 1>> $file_name
+		echo "" 1>> $file_name
+		echo "file: words$i" 1>> $file_name
+		echo "" 1>> $file_name
 
-		(time ./WordPercentSort hSF9RITZYq words$i.txt) >> $file_name 2>&1
-		(time ./WordPercentSort OHx1ZEHiht words$i.txt) 2>> $file_name 1> /dev/null
-		(time ./WordPercentSort 9cBqvgN0Qv words$i.txt) 2>> $file_name 1> /dev/null
-		(time ./WordPercentSort c0FnTkWjuL words$i.txt) 2>> $file_name 1> /dev/null
-		(time ./WordPercentSort Yl69pHrgvy words$i.txt) 2>> $file_name 1> /dev/null
-
+		(time $executable ${base_strings[0]} words$i.txt) >> $file_name 2>&1
+		(time $executable ${base_strings[1]} words$i.txt) 2>> $file_name 1> /dev/null
+		(time $executable ${base_strings[2]} words$i.txt) 2>> $file_name 1> /dev/null
+		(time $executable ${base_strings[3]} words$i.txt) 2>> $file_name 1> /dev/null
+		(time $executable ${base_strings[4]} words$i.txt) 2>> $file_name 1> /dev/null
 	done
-
 done
-
