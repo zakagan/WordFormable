ac2a541f83c07234e131ad5129c244abf232cfc4
Zachary Kagan <zakagan@gmail.com>
2016-08-21 02:45:07 -0500

    testing to fix PowerHP segfault errors

--- src/HashMapInts.c
+++ src/HashMapInts.c
@@ -34,7 +34,9 @@ void hashMapUpdate(HashMap* m, const char* str)
 {
     size_t h;
     h = communicativeStringHash(str) % m->size;
-    m->map[h]='\1';     //Sets 1 byte size validity element equal to an unsigned char literal (or close enough) '\1'
+    if (!m->map[h]) {
+        m->map[h]=(unsigned char) 1;     //Sets 1 byte size validity element equal to an casted unsigned char 1
+    }
 }
 
 // returns 1 if the character table exists in hash table, returns 0 otherwise
